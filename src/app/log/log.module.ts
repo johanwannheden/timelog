bf85954ffce26a6ef2212c6e5eaa6e2bebb68a34
Johan Wannheden <johan.wannheden@gmail.com>
2020-12-02 21:38:03 +0100

    add basic menu button and remove unnecessary modules

--- src/app/log/log.module.ts
+++ src/app/log/log.module.ts
@@ -4,9 +4,6 @@ import {LogListComponent} from './log-list/log-list.component';
 import {LogRoutingModule} from './log-routing.module';
 import {CommonModule} from '@angular/common';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
@@ -15,14 +12,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {LogDurationPipe} from './pipe/log-duration.pipe';
@@ -55,9 +45,6 @@ import {logEntryReducer} from './state/log.reducer';
         // material
         CommonModule,
         MatMomentDateModule,
-        MatTabsModule,
-        MatSidenavModule,
-        MatToolbarModule,
         MatIconModule,
         MatButtonModule,
         MatListModule,
@@ -67,14 +54,7 @@ import {logEntryReducer} from './state/log.reducer';
         MatFormFieldModule,
         MatInputModule,
         MatPaginatorModule,
-        MatProgressBarModule,
-        MatCheckboxModule,
-        MatProgressSpinnerModule,
-        MatSelectModule,
-        MatCardModule,
-        MatExpansionModule,
         MatDatepickerModule,
-        MatDialogModule,
     ],
     exports: [
         YearMonthFormatDirective,
